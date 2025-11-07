4eb49c26458432ed7c77a46b144a0f97c4880c8a
Prakash-cs20b028 <Programmer52525@gmail.com>
2025-11-07 18:15:32 +0530

    Started
    Signed-off-by: Prakash-cs20b028 <Programmer52525@gmail.com>

---
+++ IntroductionToJava/src/ImportPackages/Helper.java
@@ -0,0 +1,13 @@
+package ImportPackages;
+
+public class Helper
+{
+    public void print()
+    {
+        System.out.println("This is the Helper function");
+    }
+    public static boolean  getInt()
+    {
+        return true;
+    }
+}
---
+++ IntroductionToJava/src/Main.java
@@ -0,0 +1,11 @@
+import ImportPackages.Helper;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Helper helper = new Helper();
+        helper.print();
+    }
+
+
+}
