6519ccf01d811d8590faf34c36844586fea0c47d
Prakash-cs20b028 <Programmer52525@gmail.com>
2025-11-07 18:50:52 +0530

    Played with the byte codes
    Signed-off-by: Prakash-cs20b028 <Programmer52525@gmail.com>

--- IntroductionToJava/src/Main.java
+++ IntroductionToJava/src/Main.java
@@ -3,6 +3,7 @@ import ImportPackages.Helper;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("This is the main function");
         Helper helper = new Helper();
         helper.print();
     }
