9e54cbf7cc9e18c4f91a08cfaa592f96f44b1dfd
Prakash-cs20b028 <Programmer52525@gmail.com>
2025-11-09 00:11:35 +0530

    Bazel is used to build the project
    Signed-off-by: Prakash-cs20b028 <Programmer52525@gmail.com>

---
+++ Project_With_Bazel/src/Base.java
@@ -0,0 +1,13 @@
+// package src;
+
+// import example.com.Test;
+import src.HelloWorld;
+
+public class Base {
+
+    public static void main(String[] args) {
+        System.out.println("This is the Base class.");
+        HelloWorld.main(args);
+        // Test.main(args);
+    }
+}
---
+++ Project_With_Bazel/src/example/com/Test.java
@@ -0,0 +1,8 @@
+package example.com;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("This is the Test class from example.com");
+    }
+}
