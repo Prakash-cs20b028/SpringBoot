6ee185811ffd5cd06416f777b8951a254cff07a0
Prakash-cs20b028 <Programmer52525@gmail.com>
2025-11-10 23:32:18 +0530

    Package information
    Signed-off-by: Prakash-cs20b028 <Programmer52525@gmail.com>

---
+++ Package/Bicycle.java
@@ -0,0 +1,9 @@
+
+public class Bicycle {
+
+    public int gear = 5;
+
+    public void BicycleInfo() {
+        System.out.println("Normal Bicycle with " + gear + " gears.");
+    }
+}
---
+++ Package/NormalBicycle.java
@@ -0,0 +1,12 @@
+
+public class NormalBicycle {
+
+    public static void main(String[] args) {
+
+        Bicycle bike = new Bicycle();
+        bike.gear = 7;
+        System.out.println("This is the Normal Bicycle:");
+        bike.BicycleInfo();
+
+    }
+}
