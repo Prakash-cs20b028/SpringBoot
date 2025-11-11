11aa6e956f1f4eac7446e880bc6be48f7c140194
Prakash-cs20b028 <Programmer52525@gmail.com>
2025-11-11 19:29:09 +0530

    Package and Access modifier discussed
    Signed-off-by: Prakash-cs20b028 <Programmer52525@gmail.com>

--- AccessModifier/Customer.java
+++ AccessModifier/Customer.java
@@ -1,11 +1,49 @@
 
-class Customer {
+class Bank1 {
 
-    public static void main(String args[]) {
-        Bank b = new Bank();
+    private double balance;
 
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
+
+class Customer1 {
+
+    public static void main(String[] args) {
+        Bank1 b = new Bank1();
         b.setBalance(5000.00);
         System.out.println("Balance: " + b.getBalance());
-        //System.out.println("Balance: " + b.Balance); // This line would cause a compilation error due to private access
+    }
+}
+
+class Customer2 {
+
+    public static void main(String[] args) {
+        Bank1 b = new Bank1();
+        b.setBalance(10000.00);
+        System.out.println("Balance: " + b.getBalance());
+    }
+}
+
+class Customer3 {
+
+    public static void main(String[] args) {
+        Bank1 b = new Bank1();
+        b.setBalance(20000.00);
+        System.out.println("Balance: " + b.getBalance());
+    }
+}
+
+public class Customer {
+
+    public static void main(String[] args) {
+        Customer1.main(args);
+        Customer2.main(args);
+        Customer3.main(args);
     }
 }
